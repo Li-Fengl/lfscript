fb474ef95d8dce4b1f53d097847ed601cedd828f
Li-Fengl <98276349+Li-Fengl@users.noreply.github.com>
2026-03-26 21:54:54 +0800

    Change log file path for iptable56 execution

--- installGyd.sh
+++ installGyd.sh
@@ -45,7 +45,7 @@ After=network.target iptables.service
 
 [Service]
 Type=simple
-ExecStart=/bin/sh -c "/root/iptable56 > /root/iptable56.log 2>&1"
+ExecStart=/bin/sh -c "/root/iptable56 > /var/log/iptable56.log 2>&1"
 Restart=always
 RestartSec=2
 CapabilityBoundingSet=CAP_NET_ADMIN
